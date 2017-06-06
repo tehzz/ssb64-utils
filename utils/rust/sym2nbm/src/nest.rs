8de030e3906f09e4d7acb2e4d3c04187a3b83fdf
tehzz <zgamer@gmail.com>
2017-06-06 19:52:22 -0400

    Finish nester fn
    Add tests as well

--- utils/rust/sym2nbm/src/nest.rs
+++ utils/rust/sym2nbm/src/nest.rs
@@ -1,29 +1,24 @@
 use std::io::{BufRead};
-use std::fmt;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use nemu_mem::{MemType, SymbolInfo};
 
 #[derive(Debug)]
 struct Container {
     symbols: Vec<SymbolInfo>,
-    branches: HashMap<String, Container>
+    branches: BTreeMap<String, Container>
 }
 
 impl Container {
     fn new() -> Self {
         Container {
             symbols: Vec::new(),
-            branches: HashMap::new()
+            branches: BTreeMap::new()
         }
     }
 
     fn add_symbol(&mut self, sym: SymbolInfo) {
         self.symbols.push(sym);
-    }
-
-    fn get_branch(&self, branch: &str) -> Option<&Container> {
-        self.branches.get(branch)
     }
 
     fn get_branch_mut(&mut self, branch: &str) -> Option<&mut Container> {
@@ -38,8 +33,53 @@ impl Container {
         self.branches.contains_key(branch)
     }
 
-    fn print(&self) -> String {
-        String::from("unimplemented! :(")
+    fn print(&self, indent_level: u32) -> String {
+        let mut output = String::new();
+
+        // calculate indent string
+        let indent = (0..indent_level).fold(String::new(),
+            |mut inter, _| {
+                inter.push('\t');
+
+                inter
+            });
+
+        for (scope, subcontain) in self.branches.iter() {
+            let subcon_print = subcontain.print(indent_level + 1);
+
+            // add indent
+            output.push_str(&indent);
+            // add sub branch name
+            output.push_str(scope);
+            // new line
+            output.push('\n');
+            // add output from sub-container
+            output.push_str(&subcon_print);
+        }
+
+        // then, add all SymbolInfo(s) at this indent level
+        let mut syms = self.symbols.iter()
+            .map( |sym| {
+                let mut base = sym.print();
+
+                base.insert_str(0, &indent);
+                base
+            })
+            .collect::<Vec<_>>();
+
+        syms.sort();
+
+        let mut syms_string = syms.join("\n");
+
+        // If there are symbols, add a newline to the end
+        if !syms_string.is_empty() {
+            syms_string.push('\n');
+
+            // add string of newline separated symbols (if present)
+            output.push_str(&syms_string);
+        }
+
+        output
     }
 }
 
@@ -49,7 +89,6 @@ impl Container {
     2. "Scope" : Option<usize>  - Scope Values into Name String. Put {M} scopes (input.split('.'))
                                 into the final name string for a memory address
 */
-
 pub fn nester(br: Box<BufRead>, scope: usize, nest: Option<usize>, data_filter: &str) -> String {
     // Parse each line into it's components
     let parsed_lines: Vec<(MemType, u32, Vec<String>)> = br.lines()
@@ -67,18 +106,12 @@ pub fn nester(br: Box<BufRead>, scope: usize, nest: Option<usize>, data_filter: 
             scope_and_nest_line(substrs, scope, nest, &data_filter)
         }).collect::<Vec<_>>();
 
-    println!("parsed_lines: \n{:?}\n\n", &parsed_lines);
-
     // Combine the component lines into the Container Struct
     let output_container = to_container(parsed_lines);
 
-    println!("output_container: \n{:?}\n\n", output_container);
-
     // Return string from Container.print() fn
-    // output_container.print()
-    String::from("Not finished!")
+    output_container.print(0)
 }
-
 
 fn scope_and_nest_line(pair: Vec<String>, scope: usize, nest: Option<usize>, data_filter: &str ) -> Option<(MemType, u32, Vec<String>)> {
     let mut iter = pair.iter();
@@ -106,7 +139,6 @@ fn scope_and_nest_line(pair: Vec<String>, scope: usize, nest: Option<usize>, dat
         })
         // iterator over scope vector to limit for nesting
         .iter().rev().enumerate()
-        //.inspect( | val | println!("inspect: {:?}", val))
         // fold into a final vector
         .fold(Vec::new(), | mut acc, (i, name) | {
             match nest {
@@ -126,13 +158,14 @@ fn scope_and_nest_line(pair: Vec<String>, scope: usize, nest: Option<usize>, dat
             acc
         });
 
+    // Return only if there is a valid hex address
     match hex_addr {
         Ok(hex) => Some((mem, hex, nest_name)),
         Err(_)  => None
     }
 }
 
-fn to_container ( addrs: Vec<(MemType, u32, Vec<String>)> ) -> Container {
+fn to_container( addrs: Vec<(MemType, u32, Vec<String>)> ) -> Container {
     let mut output = Container::new();
     // step through every line in vector and fold into the base container
     for &(mem_type, addr, ref split_name) in addrs.iter() {
@@ -156,100 +189,138 @@ fn to_container ( addrs: Vec<(MemType, u32, Vec<String>)> ) -> Container {
         home.add_symbol(sym);
     }
 
-    /*
-    addrs.iter()
-        .fold( &mut output,
-        | mut base_container, &(mem_type, addr, ref split_name) | {
-            let (name, scopes) = split_name.split_last().unwrap(); // TODO: remove unwrap
-            let sym = SymbolInfo::new(addr, &name, mem_type);
-
-            // Now, step through the remaining parts of the split name string to
-            // find the nested container that should hold the symbol info
-            let mut home: &mut Container = scopes.iter()
-                .fold( &mut base_container, | nest_ref, substr | {
-
-                    if nest_ref.has_branch(substr) {
-                        nest_ref.get_branch_mut(substr).unwrap()
-                    } else {
-                        nest_ref.add_branch(substr);
-                        nest_ref.get_branch_mut(substr).unwrap()
-                    }
-            });
-
-            // Add the SymbolInfo to the found Contianer
-            home.addSymbol(sym);
-
-            // Return the base container
-            base_container
-    });*/
-
     // return filled container
     output
 }
 
 
-//---Test Input and Output-------------------------------------------------------------------------
 
+//---Test Input and Output-------------------------------------------------------------------------
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::io::{BufReader};
 
-/*
+
     const INPUT: &'static str = "80400000 boot.data.hitboxFlags
 80400004 boot.render
 80400004 boot.render.model
 80171f4c boot.draw.Bobomb.prologue";
-*/
+    const ZERO_SCOPE_NONE_NEST: &'static str = "boot
+\tdata
+\t\tMEM 0x80400000: hitboxFlags
+\tdraw
+\t\tBobomb
+\t\t\tCPU 0x80171F4C: prologue
+\trender
+\t\tCPU 0x80400004: model
+\tCPU 0x80400004: render";
 
-/*
+    const ONE_SCOPE_NONE_NEST: &'static str = "boot
+\tdraw
+\t\tCPU 0x80171F4C: Bobomb.prologue
+\tCPU 0x80400004: render.model
+\tMEM 0x80400000: data.hitboxFlags
+CPU 0x80400004: boot.render";
+    const TWO_SCOPE_NONE_NEST: &'static str = "boot
+\tCPU 0x80171F4C: draw.Bobomb.prologue
+CPU 0x80400004: boot.render
+CPU 0x80400004: boot.render.model
+MEM 0x80400000: boot.data.hitboxFlags";
+const THREE_SCOPE_NONE_NEST: &'static str = "CPU 0x80171F4C: boot.draw.Bobomb.prologue
+CPU 0x80400004: boot.render
+CPU 0x80400004: boot.render.model
+MEM 0x80400000: boot.data.hitboxFlags";
+
+const ZERO_SCOPE_ZERO_NEST: &'static str = "CPU 0x80171F4C: boot.draw.Bobomb.prologue
+CPU 0x80400004: boot.render
+CPU 0x80400004: boot.render.model
+MEM 0x80400000: boot.data.hitboxFlags";
+
+const ZERO_SCOPE_ONE_NEST: &'static str = "boot
+\tCPU 0x80171F4C: draw.Bobomb.prologue
+\tCPU 0x80400004: render
+\tCPU 0x80400004: render.model
+\tMEM 0x80400000: data.hitboxFlags";
+
+const ZERO_SCOPE_TWO_NEST: &'static str = "boot
+\tdata
+\t\tMEM 0x80400000: hitboxFlags
+\tdraw
+\t\tCPU 0x80171F4C: Bobomb.prologue
+\trender
+\t\tCPU 0x80400004: model
+\tCPU 0x80400004: render";
+
+
+
     fn str_to_buf<'a>(input: &'static str) -> Box<BufRead> {
         Box::new(BufReader::new(input.as_bytes()))
     }
-
-
-    #[test]
-    fn single_nest_input(){
-        assert_eq!(nest(str_to_buf("80400000 boot.data.hitboxFlags"), 0, 0, "data"),
-                    "MEM 0x80400000: boot.data.hitboxFlags");
-
-        assert_eq!(nest(str_to_buf("80400000 boot.data.hitboxFlags"), 1, 0, "data"),
-                    "boot
-\tMEM 0x80400000: data.hitboxFlags");
-        assert_eq!(nest(str_to_buf("80400000 boot.data.hitboxFlags"), 2, 0, "data"),
-                    "boot
-\tdata
-\t\tMEM 0x80400000: hitboxFlags");
-    } */
-
 
     #[test]
     fn substrs_tests(){
         let strs = vec!["80400000".to_string(), "boot.data.1.2.3.4.hitboxFlag".to_string()];
         let expected = (MemType::RAM, 0x80400000, vec!["boot", "data", "1", "2.3.4.hitboxFlag"]);
 
-        let output = scope_and_nest_line(strs, 1, 3, ".data.").unwrap();
-        println!("{:?}", output);
+        let output = scope_and_nest_line(strs, 1, Some(3), "data").unwrap();
 
-        //assert_eq!(output.0, expected.0);
+        assert_eq!(output.0, expected.0);
         assert_eq!(output.1, expected.1);
-        assert_eq!(output.2, expected.2)
+        assert_eq!(output.2, expected.2);
     }
 
-    /*
     #[test]
-    fn to_container_test(){
-        let tups = (MemType::RAM, 0x80400000, vec!["boot", "data", "hitboxFlag"]);
-        let sym = SymbolInfo::new(tups.1, tups.2[2], tups.0);
-        let mut expect = Container::new();
+    fn single_nest_input(){
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "data").trim(),
+                    "MEM 0x80400000: boot.data.hitboxFlags");
 
-        expect.add_branch(tups.2[0]);
-        expect.get_branch_mut(tups.2[0]).unwrap().add_branch(tups.2[1]);
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(1), "data").trim(),
+                    "boot
+\tMEM 0x80400000: data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(2), "data").trim(),
+                    "boot
+\tdata
+\t\tMEM 0x80400000: hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, None, "data").trim(),
+                    "boot
+\tdata
+\t\tMEM 0x80400000: hitboxFlags");
+    }
 
-        let mut home = expect.get_branch(tups.2[0]).unwrap().get_branch_mut(tups.2[1]).unwrap();
+    #[test]
+    fn data_filter_strings() {
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "data").trim(),
+                    "MEM 0x80400000: boot.data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "").trim(),
+                    "CPU 0x80400000: boot.data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "boot").trim(),
+                    "MEM 0x80400000: boot.data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "hitbox").trim(),
+                    "CPU 0x80400000: boot.data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80400000 boot.data.hitboxFlags"), 0, Some(0), "hitboxFlags").trim(),
+                    "MEM 0x80400000: boot.data.hitboxFlags");
+        assert_eq!(nester(str_to_buf("80123456 1.2.3.4.symbol"), 0, Some(0), "1").trim(),
+                    "MEM 0x80123456: 1.2.3.4.symbol");
+    }
 
-        home.add_symbol(sym);
+    #[test]
+    fn full_nesting_scopes() {
+        assert_eq!(nester(str_to_buf(INPUT), 0, None, "data").trim(), ZERO_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 1, None, "data").trim(), ONE_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 2, None, "data").trim(), TWO_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 3, None, "data").trim(), THREE_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 10, None, "data").trim(), THREE_SCOPE_NONE_NEST);
+    }
 
-        assert_eq!(to_container(vec![tups]), expect);
-    }*/
+    #[test]
+    fn full_nesting_nests() {
+        assert_eq!(nester(str_to_buf(INPUT), 0, None, "data").trim(), ZERO_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 0, Some(0), "data").trim(), ZERO_SCOPE_ZERO_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 0, Some(1), "data").trim(), ZERO_SCOPE_ONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 0, Some(2), "data").trim(), ZERO_SCOPE_TWO_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 0, Some(3), "data").trim(), ZERO_SCOPE_NONE_NEST);
+        assert_eq!(nester(str_to_buf(INPUT), 0, Some(10), "data").trim(), ZERO_SCOPE_NONE_NEST);
+    }
+
 }
