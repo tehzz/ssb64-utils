1ea3a601de8531add136ce0f896d527abe8ac0bb
tehzz <tehzz@users.noreply.github.com>
2017-10-04 19:44:41 -0400

    Tests for resource pointers

--- utils/rust/ssbstage/src/ssbpointers.rs
+++ utils/rust/ssbstage/src/ssbpointers.rs
@@ -5,6 +5,7 @@ use byteorder::{BE, ByteOrder};
 /// value, where the first u16 is the word offset to the next link, and the second u16 is the
 /// word offset from the beginning of file for the pointer. The chain is over when the next 
 /// value is equal to 0xFFFF. 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ResPtr {
     next:   Option<u32>,
     offset: u32,
@@ -32,7 +33,7 @@ impl ResPtr {
 
 // convert to reader/Result type...
 // maybe use a HashMap instead...? Or a sorted btree?
-pub fn read_ResPtr_chain(start: usize, buffer: &[u8]) -> Vec<(usize, ResPtr)> {
+pub fn read_res_chain(start: usize, buffer: &[u8]) -> Vec<(usize, ResPtr)> {
     let mut output = Vec::new();
     let mut offset = start;
     loop {
@@ -50,4 +51,74 @@ pub fn read_ResPtr_chain(start: usize, buffer: &[u8]) -> Vec<(usize, ResPtr)> {
     }
 
     output
+}
+
+// make possible enum to encode null value?
+pub fn check_res_ptr(value: u32) -> Option<ResPtr> {
+    let possible = ResPtr::from_u32(value);
+    let offset = possible.offset;
+
+    if let Some(ref next) = possible.next {
+        // probably a null value (0x00000000) where a pointer could be
+        if *next == offset { return None }
+        // an offset of '0' for 'next' probably won't happen with Nintendo, but it is a valid case...
+        if *next == 0 { return None }
+    } 
+
+    Some(possible)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const VALID: u32         = 0x001A00FF;
+    const VALID_END: u32     = 0xFFFF0001;
+    const INVALID_NULL: u32  = 0x00000000; 
+    const INVALID_EMPTY: u32 = 0x00004530;
+
+    const VALID_PARSED: ResPtr = ResPtr {
+        next: Some((VALID & 0xFFFF0000) >> 14),
+        offset: (VALID & 0xFFFF) << 2,
+    };
+    const VALID_END_PARSED: ResPtr = ResPtr {
+        next: None,
+        offset: (VALID_END & 0xFFFF) << 2,
+    };
+
+    const VALID_BUFFER: Vec<u8> = vec![
+        0x00,0x01,0x00,0x04,
+        0x00,0x02,0x00,0xA4,
+        0x00,0x04,0x00,0x04,
+        0xAB,0xCD,0xEF,0x01,
+        0xFF,0xFF,0x12,0x34,
+    ];
+
+    #[test]
+    fn parse_valid_res_ptrs() {
+        let test_valid = ResPtr::from_u32(VALID);
+        let test_valid_end = ResPtr::from_u32(VALID_END);
+
+        assert_eq!(test_valid, VALID_PARSED, "Error directly parsing \"VALID\"");
+        assert_eq!(test_valid_end, VALID_END_PARSED, "Error directly parsing \"VALID_END\"");
+    }
+
+    #[test]
+    fn check_valid_res_ptrs() {
+        let check_valid = check_res_ptr(VALID);
+        let check_valid_end = check_res_ptr(VALID_END);
+
+        assert_eq!(check_valid, Some(VALID_PARSED), "Error checking for valid res_ptr \"VALID\"");
+        assert_eq!(check_valid_end, Some(VALID_END_PARSED), "Error checking for valid res_ptr \"VALID_END\"");
+    }
+
+    #[test]
+    fn check_invalid_res_ptrs() {
+        let check_invalid_null = check_res_ptr(INVALID_NULL);
+        let check_invalid_empty = check_res_ptr(INVALID_EMPTY);
+
+        assert_eq!(check_invalid_null, None, "Error checking for invalid res_ptr \"INVALID_NULL\"");
+        assert_eq!(check_invalid_empty, None, "Error checking for invalid res_ptr \"INVALID_EMPTY\"");
+    }
+
 }
